49d60ebf2e1811fd6fb5e93c44ab65d0cf45f4d9
UnNolan <134528529+UnNolan@users.noreply.github.com>
2024-10-01 14:09:38 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import eventlet
-eventlet.monkey_patch()
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from astropy import units as u
@@ -278,4 +275,4 @@ if __name__ == '__main__':
     thread = threading.Thread(target=update_orbit_data)
     thread.daemon = True
     thread.start()
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=False)
+    socketio.run(app, host='0.0.0.0', port=int(os.getenv("PORT", 5000)), debug=False)
